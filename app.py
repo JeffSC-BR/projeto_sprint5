275e4ee33253515e06f54ae6072127acfcc6ea9c
JeffSC-BR <jeffersonworkon@gmail.com>
2025-02-02 20:20:13 -0500

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+car_data = pd.read_csv('vehichles.csv')
+hist_button = st.header('Criar Histograma')
+
+if hist_button:
+
+    st.write(
+        'Criando um histograma para o conjunto de dados de anúncios de vendas de carros.')
+
+    fig = px.histogram(car_data, x="odometer")
+
+    st.plotly_chart(fig, use_container_width=True)
+
+build_histogram = st.checkbox('Criar um histograma')
+
+if build_histogram:
+
+    st.write('Criando um histograma para a coluna odometer')
+
+    fig.px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
